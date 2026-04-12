331968dfcd0f7aeb1d7b3e131b27c5d9432fdb41
fengjie <qq1489869919@gmail.com>
2026-04-13 00:37:45 +0800

    Changes  准备删除小区

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
-    },
-    mobile: {
-      entry: 'src/mobile/main.js',
-      template: 'public/mobile.html',
-      filename: 'mobile.html'
-    }
-  },
   devServer: {
     port: 8081,
     historyApiFallback: true,
